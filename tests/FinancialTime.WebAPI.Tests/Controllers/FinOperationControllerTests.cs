bc14ad68c4f1e71ae3bec10a7162574e4779648b
Minato <umi0n3@gmail.com>
2023-07-03 18:05:05 +0200

    tests

---
+++ tests/FinancialTime.WebAPI.Tests/Controllers/FinOperationControllerTests.cs
@@ -0,0 +1,142 @@
+﻿using AutoMapper;
+using FinancialTime.Core.Interfaces;
+using FinancialTime.Core.Models;
+using FinancialTime.WebAPI.Controllers;
+using FinancialTime.WebAPI.DTOs.FinOperation;
+using FinancialTime.WebAPI.Mappers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FinancialTime.WebAPI.Tests.Controllers;
+
+public class FinOperationControllerTests
+{
+    private readonly MapperConfiguration _mockOperationMapper;
+
+    public FinOperationControllerTests()
+    {
+        _mockOperationMapper = new MapperConfiguration(config =>
+            config.AddProfile(new FinOperationProfile()));
+    }
+
+    [Fact]
+    public async Task GetOperationsOkResultTest()
+    {
+        // Arrange
+        var mockOperationService = new Mock<IOperationService>();
+        mockOperationService
+            .Setup(x => x.GetAllAsync())
+            .ReturnsAsync(MockData.GetOperations());
+        
+        var controller = new FinOperationController(mockOperationService.Object, _mockOperationMapper.CreateMapper());
+
+        // Act
+        var actionResult = await controller.GetOperationsAsync();
+        var result = actionResult.Result as OkObjectResult;
+        var resultValue = result!.Value as IEnumerable<FinOperationDto>;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.StatusCode.Should().Be(200);
+        resultValue.Should().HaveCount(5);
+    }
+    
+    [Fact]
+    public async Task GetOperationsNoContentResultTest()
+    {
+        // Arrange
+        var mockOperationService = new Mock<IOperationService>();
+        mockOperationService
+            .Setup(x => x.GetAllAsync())
+            .ReturnsAsync(new List<FinOperation>());
+
+
+        var controller = new FinOperationController(mockOperationService.Object, _mockOperationMapper.CreateMapper());
+
+        // Act
+        var actionResult = await controller.GetOperationsAsync();
+        var result = actionResult.Result as NoContentResult;
+
+        // Assert
+        result!.StatusCode.Should().Be(204);
+    }
+    
+    [Fact]
+    public async Task GetOperationByIdOkResultTest()
+    {
+        // Arrange
+        var mockOperationService = new Mock<IOperationService>();
+        mockOperationService
+            .Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+            .ReturnsAsync(It.IsAny<FinOperation>());
+
+
+        var controller = new FinOperationController(mockOperationService.Object, _mockOperationMapper.CreateMapper());
+
+        // Act
+        var actionResult = await controller.GetOperationByIdAsync(It.IsAny<int>());
+        var result = actionResult.Result as OkObjectResult;
+
+        // Assert
+        result.Should().NotBeNull();
+        result!.StatusCode.Should().Be(200);
+    }
+    
+    [Fact]
+    public async Task AddOperationOkResultTest()
+    {
+        // Arrange
+        var mockOperationService = new Mock<IOperationService>();
+
+        var controller = new FinOperationController(mockOperationService.Object, _mockOperationMapper.CreateMapper());
+
+        // Act
+        var actionResult = await controller.AddOperationAsync(It.IsAny<FinOperationAddDto>());
+        var result = actionResult.Result as OkObjectResult;
+
+        // Assert
+        mockOperationService.Verify(x => x.AddAsync(It.IsAny<FinOperation>()), Times.Once);
+        
+        result.Should().NotBeNull();
+        result!.StatusCode.Should().Be(200);
+    }
+    
+    [Fact]
+    public async Task EditOperationOkResultTest()
+    {
+        // Arrange
+        var mockOperationService = new Mock<IOperationService>();
+
+        var controller = new FinOperationController(mockOperationService.Object, _mockOperationMapper.CreateMapper());
+
+        // Act
+        var actionResult = await controller.EditOperationAsync(It.IsAny<int>(), MockData.GetOperationEditDto());
+        var result = actionResult as OkResult;
+
+        // Assert
+        mockOperationService.Verify(x => x.EditAsync(It.IsAny<FinOperation>()), Times.Once);
+        
+        result.Should().NotBeNull();
+        result!.StatusCode.Should().Be(200);
+    }
+    
+    [Fact]
+    public async Task DeleteOperationOkResultTest()
+    {
+        // Arrange
+        var mockOperationService = new Mock<IOperationService>();
+
+        var controller = new FinOperationController(mockOperationService.Object, _mockOperationMapper.CreateMapper());
+
+        // Act
+        var actionResult = await controller.DeleteOperationAsync(It.IsAny<int>());
+        var result = actionResult as OkResult;
+
+        // Assert
+        mockOperationService.Verify(x => x.DeleteAsync(It.IsAny<FinOperation>()), Times.Once);
+        
+        result.Should().NotBeNull();
+        result!.StatusCode.Should().Be(200);
+    }
+}
---
+++ tests/FinancialTime.WebAPI.Tests/MockData.cs
@@ -0,0 +1,125 @@
+﻿using FinancialTime.Core.Enums;
+using FinancialTime.Core.Models;
+using FinancialTime.WebAPI.DTOs.FinOperation;
+
+namespace FinancialTime.WebAPI.Tests;
+
+public static class MockData
+{
+    public static IEnumerable<FinOperation> GetOperations()
+    {
+        var result = new List<FinOperation>
+        {
+            new()
+            {
+                Id = 1,
+                Value = 4000.12M,
+                Date = new DateTime(2023, 6, 10),
+                FinTypeId = 1,
+                FinType = new FinType
+                {
+                    Id = 1,
+                    Budget = EBudget.Income,
+                    Name = "Salary"
+                }
+            },
+            new()
+            {
+                Id = 2,
+                Value = 1000.5M,
+                Date = new DateTime(2023, 6, 10),
+                FinTypeId = 2,
+                FinType = new FinType
+                {
+                    Id = 2,
+                    Budget = EBudget.Expense,
+                    Name = "Products"
+                }
+            },
+            new()
+            {
+                Id = 3,
+                Value = 133M,
+                Date = new DateTime(2023, 6, 10),
+                FinTypeId = 3,
+                FinType = new FinType
+                {
+                    Id = 3,
+                    Budget = EBudget.Expense,
+                    Name = "Fun"
+                }
+            },
+            new()
+            {
+                Id = 4,
+                Value = 100M,
+                Date = new DateTime(2023, 6, 10),
+                FinTypeId = 4,
+                FinType = new FinType
+                {
+                    Id = 4,
+                    Budget = EBudget.Expense,
+                    Name = "Restaurants"
+                }
+            },
+            new()
+            {
+                Id = 5,
+                Value = 150M,
+                Date = new DateTime(2023, 6, 11),
+                FinTypeId = 4,
+                FinType = new FinType
+                {
+                    Id = 4,
+                    Budget = EBudget.Expense,
+                    Name = "Restaurants"
+                }
+            },
+        };
+
+        return result;
+    }
+
+    public static FinOperationEditDto GetOperationEditDto()
+    {
+        return new FinOperationEditDto
+        {
+            Value = 500,
+            Date = DateTime.Today,
+            FinTypeId = 4
+        };
+    }
+    
+    public static IEnumerable<FinType> GetTypes()
+    {
+        var result = new List<FinType>()
+        {
+            new()
+            {
+                Id = 1,
+                Budget = EBudget.Income,
+                Name = "Salary"
+            },
+            new()
+            {
+                Id = 2,
+                Budget = EBudget.Expense,
+                Name = "Products"
+            },
+            new()
+            {
+                Id = 3,
+                Budget = EBudget.Expense,
+                Name = "Fun"
+            },
+            new()
+            {
+                Id = 4,
+                Budget = EBudget.Expense,
+                Name = "Restaurants"
+            },
+        };
+
+        return result;
+    }
+}
