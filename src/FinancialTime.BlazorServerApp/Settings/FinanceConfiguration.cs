8e66cdeaaa3fff89daf32e2abfacb0120349c39d
Minato <umi0n3@gmail.com>
2023-07-08 13:35:28 +0200

    config

---
+++ src/FinancialTime.BlazorServerApp/Settings/FinanceConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace FinancialTime.BlazorServerApp.Settings;
+
+public class FinanceConfiguration
+{
+    public static readonly string Configuration = "FinanceConfiguration";
+    
+    public string? OperationUrl { get; set; }
+    
+    public string? TypeUrl { get; set; }
+    
+    public string? ReportUrl { get; set; }
+}
