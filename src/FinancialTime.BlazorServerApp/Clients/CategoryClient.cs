9e33cf74cd277466e346d259bbfa4716a8717eab
Minato <umi0n3@gmail.com>
2023-07-14 18:59:21 +0200

    check category only

--- src/FinancialTime.BlazorServerApp/Clients/CategoryClient.cs
+++ src/FinancialTime.BlazorServerApp/Clients/CategoryClient.cs
@@ -1,6 +1,4 @@
-﻿using System.Net;
-using FinancialTime.BlazorServerApp.Interfaces;
-using FinancialTime.BlazorServerApp.Settings;
+﻿using FinancialTime.BlazorServerApp.Interfaces;
 using FinancialTime.Core.DTOs.FinType;
 using Newtonsoft.Json;
 
@@ -21,21 +19,41 @@ public class CategoryClient : ICategoryClient
     {
         var response = await _httpClient.GetAsync(_finTypeUrl);
 
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new BadHttpRequestException($"Status code: {response.StatusCode}");
+        }
+        
         return JsonConvert.DeserializeObject<IEnumerable<FinTypeDto>>(await response.Content.ReadAsStringAsync());
     }
 
     public async Task AddAsync(FinTypeAddDto item)
     {
         var response = await _httpClient.PostAsJsonAsync(_finTypeUrl, item);
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new BadHttpRequestException($"Status code: {response.StatusCode}");
+        }
     }
 
     public async Task UpdateAsync(int id, FinTypeEditDto item)
     {
         var response = await _httpClient.PutAsJsonAsync($"{_finTypeUrl}{id}", item);
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new BadHttpRequestException($"Status code: {response.StatusCode}");
+        }
     }
 
     public async Task DeleteAsync(int id)
     {
         var response = await _httpClient.DeleteAsync($"{_finTypeUrl}{id}");
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new BadHttpRequestException($"Status code: {response.StatusCode}");
+        }
     }
 }
